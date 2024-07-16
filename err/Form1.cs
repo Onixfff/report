b8d98b5bf7a54ba370e588d944086bfb1771d9b0
Onixfff <danilnikonov01@mail.ru>
2024-07-16 18:53:22 +0200

    Убрал коменты и using

--- err/Form1.cs
+++ err/Form1.cs
@@ -2,24 +2,18 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Windows.Forms;
-//using MySql.Data.MySqlClient;
 using MySql.Data.MySqlClient;
-//using MySql;
-using System.Threading;
 using DataUpdater;
-//using Microsoft.Office.Interop.Excel;
 
 namespace err
 {
     public partial class Form1 : Form
     {
        MySqlConnection mCon = new MySqlConnection("Database=spslogger; Server=192.168.100.7; port=3306; username=%user_2; password=20112004; charset=utf8");
-        //MySqlConnection mCon = new MySqlConnection("Database=spslogger; Server=localhost; port=3306; username=root; password=6180; charset=utf8 ");
         MySqlCommand msd;
         MySqlConnection mconServer = new MySqlConnection("Database=spslogger; Server=192.168.100.100; port=3306; username=D_user; password=Aeroblock12345%; charset=utf8");
         string consql = "Database=spslogger; Server=192.168.100.100; port=3306; username=D_user; password=Aeroblock12345%; charset=utf8";
         string conSQL = "Database=spslogger; Server=192.168.100.7; port=3306; username=%user_2; password=20112004; charset=utf8";
-        //string conSQL = "Database=spslogger; Server=localhost; port=3306; username=root; password=6180; charset=utf8  ";
         
         public Form1()
         {
